a1105d6e11f850fdf3e4833e7d867ebfe5d1fba9
Таджибаев Дамир Гуламович <Tadzhibaev_DG@oemz.ru>
2019-12-04 19:51:55 +0600

    Dz6_home2

--- src/Dz6/Client/Client.java
+++ src/Dz6/Client/Client.java
@@ -1,4 +1,77 @@
 package Dz6.Client;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    public Client(String ipAddress, int port){
+        Scanner scan = new Scanner(System.in);
+        try {
+            socket = new Socket(ipAddress, port);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            Resender resend = new Resender();
+            resend.start();
+
+            String str = "";
+            while (!str.equals("/exit")) {
+                str = scan.nextLine();
+                out.writeUTF(str);
+            }
+
+            System.out.println("Тормозим поток");
+            resend.setStopService();
+            resend.join();
+            System.out.println("Поток остановлен");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            close();
+        }
+    }
+
+    private void close() {
+        try {
+            System.out.println("Закрываем сокет");
+            in.close();
+            out.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class Resender extends Thread{
+        private boolean stopService;
+
+        public void setStopService() {
+            stopService = true;
+            System.out.println("Клиент будет закрыт");
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (!stopService){
+                    String str = in.readUTF();
+                    System.out.println(str);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Поток закрыт");
+        }
+    }
+
 }
--- src/Dz6/MainStart.java
+++ src/Dz6/MainStart.java
@@ -1,4 +1,30 @@
 package Dz6;
 
+import Dz6.Client.Client;
+import Dz6.Server.Server;
+
+import java.util.Scanner;
+
 public class MainStart {
+    public static void main(String[] args) {
+        int port = 9999;
+        String ipAddress = "localhost";
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("В каком режиме будем работать? \n S(erver) / C(lient)");
+            char answer = Character.toLowerCase(in.nextLine().charAt(0));
+            switch (answer){
+                case ('s'):
+                    new Server(port);
+                    break;
+                case ('c'):
+                    new Client(ipAddress, port);
+                    break;
+                default:
+                    System.out.printf("Не правильный выбор. Повторите");
+                    break;
+            }
+        }
+    }
 }
